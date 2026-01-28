746106fc707cd6d24fa1058f5a30794c4dda421d
555karar4-del <555karar4@gmail.com>
2026-01-28 21:35:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ import ffmpeg
 load_dotenv()
 
 TOKEN = "8352548859:AAGxEI9yk_4TZwHO9UFZ5A7AhNU3YlvD2hQ"            # من BotFather
-OWNER_ID = "6538981552"  # ايديك التليجرام للتحكم
+OWNER_ID = 6538981552  # ايديك التليجرام للتحكم
 MAX_FILE_SIZE = 50 * 1024 * 1024            # حد تليجرام 50 ميجا
 SUPPORTED_DOMAINS = {
     "tiktok.com", "www.tiktok.com", "vm.tiktok.com",
